d8fe0bc86954917441f30acd3078f14db90c7b71
Mailan Reiser <iammai@gmail.com>
2015-05-11 22:16:42 -0700

    [dropdown] cleanup on dropdown

--- app/components/dropdown/dropdownDirective.js
+++ app/components/dropdown/dropdownDirective.js
@@ -4,16 +4,13 @@ Application.Directives.directive("dropdownMenu", function() {
         templateUrl: "app/components/dropdown/dropdown-menu.html",
         controller: function() {
             this.selectItem = function(value, $event){
-                alert(value); //ewww! Hate alerts but we're required to use it in this example! we also console log which is more gentle for debugging
                 console.log(value);
             };
             this.selectSubItem = function(value, $event){
-                alert(value);
                 console.log(value);
                 $event.stopPropagation();
             };
             this.selectSub2Item = function(value, $event){
-                alert(value);
                 console.log(value);
                 $event.stopPropagation();
             };
--- vs-jquery-dropdown/dropdown_menu.js
+++ vs-jquery-dropdown/dropdown_menu.js
@@ -44,44 +44,14 @@ var MENU = [
     var Menu = {
         init: function() {
             this.menuSpace = $('#menu-space');
-            this.createMenuHtml();
             this.menuItems = $('div.unit');
             this.bindEvents();
-        },
-
-        //createMenuHtml appends the Dropdown HTML from the MENUJson, this is not needed if we simply know and place static html in index.html instead
-        createMenuHtml: function() {
-            for (var i in MENU) {
-                var title = MENU[i].title;
-                var mainBar = $("<div class='menu-item unit' data-title='" + title + "'>" + title + "</div>");
-                this.menuSpace.append(mainBar);
-
-                if (MENU[i].submenu && MENU[i].submenu !== null) {
-                    mainBar.addClass('has-submenu');
-                    for (var k in MENU[i].submenu) {
-                        var title = MENU[i].submenu[k].title;
-                        var subBar = $("<div class='submenu-item unit' data-title='" + title + "'>" + title + "</div>");
-                        mainBar.append(subBar);
-
-
-                        if (MENU[i].submenu[k].submenu && MENU[i].submenu[k].submenu !== null) {
-                            subBar.addClass('has-sub2menu');
-                            for (var j in MENU[i].submenu[k].submenu) {
-                                var title = MENU[i].submenu[k].submenu[j].title
-                                var sub2Bar = $("<div class='sub2menu-item unit' data-title='" + title + "'>" + title + "</div>");
-                                subBar.append(sub2Bar);
-                            }
-                        }
-                    }
-                }
-            }
         },
 
         bindEvents: function() {
             var self = this;
             self.menuItems.bind('click', function(event){
-                //binds events to only the parent, not the children nodes
-                if (event.target !== this) return;
+                event.stopPropagation();
 
                 var unit = $(event.target);
                 self.displayAlert(unit.attr('data-title'));
@@ -92,7 +62,6 @@ var MENU = [
             alert(displayText); // Alerts! Bad form! Eep! console.log is much more kind.
             console.log(displayText);
         }
-
 
 
     }
